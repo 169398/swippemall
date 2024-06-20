cff0d23c09f3beae3b19170644c15f2586b8d5d0
169398 <kulubiidris@gmail.com>
2024-06-20 12:17:55 +0300

    Added the theme provoider and the favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,27 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider"
+import { MantineProvider } from '@mantine/core';
+import { Analytics } from '@vercel/analytics/react';
+
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "swippemall",
-  description: "e-commerce application for students in campus",
+  title: "Create your online shop today",
+  description: "Easy and efficient buying and selling of goods on cumpus",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+
+  twitter: {
+    card: 'summary_large_image',
+    title:
+      'swipe- create your own shop and sell goods to a large audience and at a lower cost',
+    description:
+      'Quickly  set-up your shop and upload the goods you are selling  to other students conviniently.',
+    
+  }
 };
 
 export default function RootLayout({
@@ -16,7 +31,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <MantineProvider >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Analytics />
+
+          </ThemeProvider>
+          </MantineProvider>
+        </body>
     </html>
   );
 }
